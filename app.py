25c7b816d23773f2a0bd2f2f7467d0fec0418578
forkkr <kashobroy@gmail.com>
2019-07-18 17:45:58 +0600

    Kashob v1.2

--- app.py
+++ app.py
@@ -4,7 +4,32 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def home():
+    return render_template('index.html', **locals())
+
+
+@app.route('/services')
+def services():
+    return render_template('index.html', **locals())
+
+
+@app.route('/latest-updates')
+def latest_updates():
+    return render_template('index.html', **locals())
+
+
+@app.route('/recent-reports')
+def recent_reports():
+    return render_template('index.html', **locals())
+
+
+@app.route('/my-team')
+def my_team():
+    return render_template('index.html', **locals())
+
+
+@app.route('/about')
+def about():
     return render_template('index.html', **locals())
 
 
